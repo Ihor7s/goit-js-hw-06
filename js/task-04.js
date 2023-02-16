23d5a990f9b05c0829fa5af0dd0339737fd3606a
Ihor7s <sapishchuk.igor@gmail.com>
2023-02-16 23:59:58 +0200

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,26 +1,41 @@
-const counterValue = 0;
-
-const decrementBtn = document.querySelector('button[data-action="decrement"]');
-// console.log(decrementBtn);
-
-const incrementBtn = document.querySelector('button[data-action="increment"]');
-// console.log(incrementBtn);
-
-decrementBtn.addEventListener('click', () => {
-    console.log('klik-1')
-})
-
-incrementBtn.addEventListener('click', () => {
-    
-    console.log('klik+1')
-})
 
 
+const value = document.querySelector("#value");
+const counterBtn = document.querySelector("#counter");
+
+
+const buttonDecrement = counterBtn.firstElementChild;
+const buttonIncrement = counterBtn.lastElementChild;
+
+let counterValue = 0;
+
+const valueСounter = function () {
+  value.textContent = counterValue;
+};
+
+const counterDecrement = function () {
+  counterValue -= 1;
+  valueСounter();
+};
+
+const counterIncrement = function () {
+  counterValue += 1;
+  valueСounter();
+};
+
+buttonDecrement.addEventListener("click", (event) => {
+  counterDecrement();
+});
+
+buttonIncrement.addEventListener("click", (event) => {
+  counterIncrement();
+});
 
 
 
 
 
+// let counterValue = 0;
 
 // const decrementBtn = document.querySelector('button[data-action="decrement"]');
 // // console.log(decrementBtn);
@@ -33,7 +48,6 @@ incrementBtn.addEventListener('click', () => {
 // })
 
 // incrementBtn.addEventListener('click', () => {
+    
 //     console.log('klik+1')
 // })
-
-
