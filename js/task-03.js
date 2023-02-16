49d763925dcffe9003ae9e848090fa060fc15fb6
Ihor7s <sapishchuk.igor@gmail.com>
2023-02-16 23:41:43 +0200

    task3-finish

--- js/task-03.js
+++ js/task-03.js
@@ -13,33 +13,24 @@ const images = [
   },
 ];
 
-const liRef = document.createElement('li');
+const galleryImg = document.querySelector(".gallery");
 
-const imgRef = document.createElement('img');
-imgRef.classList.add = ('ithem__img');
-imgRef.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-imgRef.alt = 'White and Black Long Fur Cat';
+const makeGallery= (image) => {
+  const { url, alt } = image;
+  return `<li class = "gallery-list"><img class = "gallery-list__ithem" src ="${url}" alt = "${alt}"></li>`;
+};
 
-liRef.append(imgRef);
-console.log(liRef);
+const makeGalImg = images.map(makeGallery).join(" ");
+
+galleryImg.insertAdjacentHTML("beforeend", makeGalImg);
 
 
 
+// const liRef = document.createElement('li');
+// const imgRef = document.createElement('img');
+// imgRef.classList.add = ('ithem__img');
+// imgRef.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+// imgRef.alt = 'White and Black Long Fur Cat';
 
-//111111
-// const ithemEl = document.createElement('li');
-// ithemEl.classList.add('site-nav')
-
-// const imageEl = document.createElement('img');
-// imageEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-// imageEl.alt = 'White and Black Long Fur Cat';
-// imageEl.classList.add('site-nav__link')
-// ithemEl.appendChild(imageEl);
-
-// console.log(ithemEl);
-//////////////////////////
-
-// const ithemmEl = document.guerySelector('.gallery');
-// ithemmEl.insertAdjacentHTML('beforeend', '<li><li>');
-
-// console.log(ithemmEl);
+// liRef.append(imgRef);
+// console.log(liRef);
