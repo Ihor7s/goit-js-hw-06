091b91bb15d1351bb95f5647401a14ab1f97f86f
Ihor7s <sapishchuk.igor@gmail.com>
2023-02-19 09:13:55 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChange = document.querySelector('.change-color');
+buttonChange.addEventListener("click", getRandomHexColor);
+
+const backgroundColor = document.querySelector('body');
+backgroundColor.style.background = getRandomHexColor();
+
+const spanColor = document.querySelector('.color');
+spanColor.textContent = getRandomHexColor();
+
